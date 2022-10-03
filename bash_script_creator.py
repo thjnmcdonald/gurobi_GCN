99e5878f5a7f34e97d4db280cf4174b724c6ecb6
thjnmcdonald <t.h.j.n.mcdonald@students.uu.nl>
2022-10-03 11:30:14 +0200

    clean up before 10 hour experiment

--- bash_script_creator.py
+++ bash_script_creator.py
@@ -1,12 +1,10 @@
-location_list = ['2022-08-31_12:00:14-GCN_16_1',
- '2022-08-31_12:01:19-GCN_32_1',
- '2022-08-31_12:02:01-GCN_64_1',
- '2022-08-31_12:38:58-GCN_16_2',
+location_list = ['2022-08-31_12:33:27-GCN_32_1',
+ '2022-08-31_12:03:07-GCN_16_2',
  '2022-08-31_12:04:42-GCN_32_2',
  '2022-08-31_12:45:57-GCN_64_2',
- '2022-08-31_12:06:21-GCN_16_3',
- '2022-08-31_12:57:18-GCN_32_3',
- '2022-08-31_13:02:07-GCN_64_3']
+ '2022-08-31_12:53:27-GCN_16_3',
+ '2022-08-31_12:57:55-GCN_32_3',
+ '2022-08-31_13:01:54-GCN_64_3']
 
 def create_bash_script(location_list):
     print('SECONDS=0')
@@ -14,10 +12,10 @@ def create_bash_script(location_list):
     print(f'now=$(date +"%T")')
     print(f'echo \"Started at : $now\"')
 
-    mol_len_list = [8]
+    mol_len_list = [4, 6, 8]
     for j, mol_len in enumerate(mol_len_list):
         for i, location in enumerate(location_list):
-            print(f'python GCN_MILP_14_features.py --location {location} --time_lim 10800 --mol_len {mol_len} > GCN_outputs/{location[-8:]}_mol_len_{mol_len}.txt')
+            print(f'python GCN_MILP_14_features.py --location {location} --time_lim 36000 --mol_len {mol_len} > GCN_outputs/{location[-8:]}_mol_len_{mol_len}.txt')
             print(f'echo \"progress: {(j)*len(location_list) + i + 1}/{(len(location_list))*(len(mol_len_list))} after: $SECONDS s\"')
             print(f'SECONDS=0')
             print(f'now=$(date +"%T")')
